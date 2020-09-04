6057bc078f33e0f692745c316512dbc582bc7c05
Jose Moran <3005377270101@ingenieria.usac,edu.gt>
2020-09-03 22:06:44 -0600

    Implementacion de botones y delay

--- Proyecto1/Proyecto1.ino
+++ Proyecto1/Proyecto1.ino
@@ -2,69 +2,69 @@
 #define SERVO 9
 #define BUZZER 29
 #define BOTONSERVO 23
+#define BOTONCERRAR 33
 Servo servoMotor;
-long tiempoSubida = 0;
-long tiempoBuzzer=0;
+long tiempoSubida;
 bool sube = false;
 bool regreso = true;
-bool prenderBuzzer = false;
+bool ejecuto;
+bool buzzer = false;
 void setup() {
 
   Serial.begin(9600);
   pinMode(25, OUTPUT);
   pinMode(27, OUTPUT);
+
   pinMode(BOTONSERVO, INPUT);
+  pinMode(BOTONCERRAR, INPUT);
   pinMode(BUZZER, OUTPUT);
 
   servoMotor.attach(SERVO);
 
   servoMotor.write(0);
+  ejecuto = false;
 }
 
 void loop() {
   int leer = digitalRead(BOTONSERVO);
-  if (!regreso && leer != 1) {
-    sube = true;
-  }
 
-  if (leer == 1 && regreso) {
+  if (leer == 1) {
     for (int i = 0; i <= 359; i++)
     {
       servoMotor.write(i);
     }
-    tiempoSubida = millis();
+    ejecuto = true;
 
-    regreso = false;
     digitalWrite(27, HIGH);
     digitalWrite(25, LOW);
   }
 
-  if (millis() - tiempoSubida > 6000 || leer == 1 && sube) {
+  if (ejecuto) {
+    bool boton = false;
+    tiempoSubida = millis();
+
+    while (millis() - tiempoSubida < 7000 &&  boton == false) {
+      boton = digitalRead(BOTONCERRAR);
+      if (boton == 1) {
+        break;
+      }
+    }
+
 
     for (int i = 359; i >= 0; i--)
     {
       servoMotor.write(i);
     }
-    prenderBuzzer = true;
-   regreso = false;
-  }
-
-  if(prenderBuzzer && servoMotor.read()==0){
-     tiempoBuzzer = millis();
-    digitalWrite(BUZZER, HIGH);
-    digitalWrite(25, HIGH);
-    digitalWrite(27, LOW);
-    sube = false;
-    regreso = false;
-    prenderBuzzer = false;
-  }
-
- if (millis() - tiempoBuzzer > 3000) {
+    delay(3000);
+    long tiempoBuzzer = millis();
+    ejecuto = false;
+    while (millis() - tiempoBuzzer < 3000) {
+      digitalWrite(BUZZER, HIGH);
+      digitalWrite(25, HIGH);
+      digitalWrite(27, LOW);
+    }
     digitalWrite(BUZZER, LOW);
     digitalWrite(25, LOW);
     digitalWrite(27, LOW);
-    tiempoSubida = 0;
-    tiempoBuzzer = 0;
-    regreso = true;
   }
 }
