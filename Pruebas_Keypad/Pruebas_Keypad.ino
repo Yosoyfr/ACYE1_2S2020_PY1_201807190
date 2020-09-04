d5aa80a70a2f618754af7bd51a00162ac2da0e52
Francisco Suarez <39706929+Yosoyfr@users.noreply.github.com>
2020-09-03 23:12:27 -0600

    [KEYPAD] Problemas en los atributos de la estructura de usuarios

--- Pruebas_Keypad/Pruebas_Keypad.ino
+++ Pruebas_Keypad/Pruebas_Keypad.ino
@@ -34,9 +34,42 @@ void setup()
   Serial.begin(9600);
   displayscreen();
   lcd.begin(16, 2);
-  Serial.println("Reconocio basura");
+  //clearEEPROM();
 
-  //Serial.println(EEPROM[0]);
+  int a = 0;
+  EEPROM.put(0, a);
+
+  char id[]= "1234";
+  char contra[] = "12345678";
+  
+
+  user usuario1 = {"1234","12345678"};
+  Serial.println(usuario1.id);
+  Serial.println(usuario1.password);
+  //addUser(usuario1);
+
+  Serial.println(contadorUsuarios());
+
+  int address = 2;
+  for (int i = 0 ; i < contadorUsuarios() ; ++i) {
+    user usuario;
+    EEPROM.get(address, usuario);
+    Serial.print("ID: ");
+    Serial.print(usuario.id);
+    Serial.print(" Contrasenia: ");
+    Serial.println(usuario.password);
+
+    address += sizeof(user);
+  }
+  /*
+    for (int i = 0 ; i < EEPROM.length()  ; ++i) {
+      Serial.print("EEPROM Data: ");
+      Serial.print(i);
+      Serial.print(" Value: ");
+
+      Serial.println(EEPROM[i]);
+    }
+  */
 }
 
 void loop()
@@ -79,6 +112,18 @@ void loop()
   }
 }
 
+void clearEEPROM()
+{
+  Serial.println("EEPROM");
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    if (EEPROM.read(i) != 0)                    //skip already "empty" addresses
+    {
+      EEPROM.write(i, 0);                       //write 0 to address i
+    }
+  }
+  Serial.println("EEPROM erased");                            //reset address counter
+}
+
 void incorrect()
 {
   memset(pass, '\0', 4);
@@ -107,6 +152,29 @@ void correct()
   lcd.clear();
   displayscreen();
 }
+
+/*
+   Metodo que recupera el numero de usuarios registrados en el sistema
+   de la memeria EEPROM del arduino, la cual serian las primeras dos posiciones
+*/
+int contadorUsuarios() {
+  int nUsuarios;
+  EEPROM.get(0, nUsuarios);
+  return nUsuarios;
+}
+/*
+   Metodo para insertar un nuevo usuario del sistema a la memoria EEPROM
+*/
+void addUser(user usuario) {
+  //Ingresamos el nuevo usuario
+  int nUsuarios = contadorUsuarios();
+  int index = nUsuarios * sizeof(user) + 2;
+  EEPROM.put(index, usuario);
+  //Aumentamos el contador de usuarios
+  nUsuarios++;
+  EEPROM.put(0, nUsuarios);
+}
+
 
 void displayscreen()
 {
